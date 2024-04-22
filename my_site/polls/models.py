caa5473cd838a0150517f362ba9c4582065b7c30
xivni1337 <sasahomic79@gmail.com>
2024-04-22 20:44:24 +0300

    22.04

---
+++ my_site/polls/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=199)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=50)
+    votes = models.IntegerField(default=0)
